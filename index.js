dba57dbace6dc2d0cc11156887466c80b9a87541
sagala2002 <sagalawiliam0@gmail.com>
2022-09-04 09:45:11 +0700

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const cetakNama = require("./main");
+const cetakNama = require("./main"); // local moduke
+const fs = require('fs') // core module
+const moment = require('moment'); // third party module / npm module / node_modules
+const PI = require("./main");
 
-require("./main");
 
 console.log(cetakNama("wiliam")); 
-console.log(cetakNama("Test")); 
+console.log(cetakNama(PI)); 
+
--- main.js
+++ main.js
@@ -2,4 +2,7 @@ function cetakNama(nama) {
   return `hello ${nama}, kabar baik`;
 }
 
+const PI = 3.14;
+
 module.exports = cetakNama;
+module.exports = PI;
